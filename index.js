abb35f92859ddcc69db22885cb95018461a6b592
conconrob <connortprobertson@gmail.com>
2019-03-21 16:01:17 +0000

    coded and tested get zoos endpoint

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const helmet = require("helmet");
 const knex = require("knex");
+const knexConfig = require("./knexfile")
 
-const db = knex({
-  client: "sqlite3",
-  connection: { filename: "./data/lambda.sqlite3" },
-  useNullAsDefault: true,
-});
+const db = knex(knexConfig.development);
 
 const server = express();
 
@@ -14,6 +11,15 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+server.get("/api/zoos", async (req, res) => {
+  try{
+    const zoos = await db.select( '*').from('zoos');
+    res.status(200).json(zoos);
+  }catch(error){
+    res.status(400).json(error)
+  }
+})
 
 const port = 3300;
 server.listen(port, function() {
