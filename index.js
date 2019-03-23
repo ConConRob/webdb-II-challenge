c83b53b5412819ec154e5204f33508bb7030294e
conconrob <connortprobertson@gmail.com>
2019-03-23 11:58:02 +0000

    coded and tested get zoo by id route

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const helmet = require("helmet");
 const knex = require("knex");
-const knexConfig = require("./knexfile")
+const knexConfig = require("./knexfile");
 
 const db = knex(knexConfig.development);
 
@@ -13,13 +13,29 @@ server.use(helmet());
 // endpoints here
 
 server.get("/api/zoos", async (req, res) => {
-  try{
-    const zoos = await db.select( '*').from('zoos');
+  try {
+    const zoos = await db.select("*").from("zoos");
     res.status(200).json(zoos);
-  }catch(error){
-    res.status(400).json(error)
+  } catch (error) {
+    res.status(400).json(error);
   }
-})
+});
+
+server.get("/api/zoos/:id", async (req, res) => {
+  try {
+    const zoo = await db
+      .select("*")
+      .from("zoos")
+      .where("id", "=", req.params.id);
+    if (zoo.length === 0) {
+      res.status(404).json({ message: "that zoo does not exist" });
+    } else {
+      res.status(200).json(zoo);
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 const port = 3300;
 server.listen(port, function() {
